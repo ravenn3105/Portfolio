d414539ded06beb467a6a6fab8323c6477f7eca5
Riya Singh <riya.singhh3105@gmail.com>
2024-07-29 23:10:32 +0530

    added href links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -53,7 +53,7 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-400 leading-relaxed">
+              <a href="mailto:riya.singhh3105@gmail.com" className="text-indigo-400 leading-relaxed">
                 riya.singhh3105@gmail.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
